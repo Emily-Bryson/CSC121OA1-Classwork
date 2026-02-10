bbba904948730061dcb07b867af37472fbdbad9d
Emily Bryson <emilymbryson@students.abtech.edu>
2026-02-10 11:28:41 -0500

    Add files via upload

---
+++ 3-4. Guest List Sorted.py
@@ -0,0 +1,14 @@
+guests = ['Aunt B', 'Katie', 'Elijah', 'Madilyn']
+guests.insert(0, 'Ethan')
+guests.insert(2, 'Stephanie')
+guests.append('Mason')
+guests.sort()
+print (f"Hey {guests[0]} would you like to have dinner with me it'll be fun!")
+print (f"How you doin' {guests [1]} how would you like to go to dinner with me?")
+print (f"Man I have not seen you in a while, {guests [2]} would you like to join me for dinner?")
+print (f"AHHH Wifey it has been so long since we saw eachother {guests [3]} want to grab dinner sometime.") 
+print (f"What's up, {guests [4]} what do you say to dinner?")
+print (f"Boy you act just like your brother, {guests [5]} how would you like to go for dinner?")
+print (f"My girl {guests [6]} what do think about a night out for dinner ?")
+print (f"Hey guys I found a bigger dinner table so we now have room for more guests.")
+
