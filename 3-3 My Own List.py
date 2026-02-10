6a12a8774c81f5903dfef9bd2dbc038a40693fac
Emily Bryson <emilymbryson@students.abtech.edu>
2026-02-10 11:28:04 -0500

    Add files via upload

---
+++ 3-3 My Own List.py
@@ -0,0 +1,13 @@
+cars = ['mustang', 'ram', 'kia stinger', 'jeep', 'convertable', 'kia sorento']
+message_0 = f"One of the first cars I ever drove in and really liked was a {cars[0].title()}."
+message_1 = f"My dad drives a red {cars[1].title()} and we call her Ruby."
+message_2 = f"My dream car is a blue {cars[2].title()} which is a sports car."
+message_3 = f"My boyfriends mom Ginger drives a {cars[3].title()} and we call her Jolene."
+message_4 = f"I think that {cars[4].title()} are really cool especially in the summer."
+message_5 = f"My mom drives a grayish blue {cars[5].title()} and we call her Bossy."
+print(message_0)
+print(message_1)
+print(message_2)
+print(message_3)
+print(message_4)
+print(message_5)
