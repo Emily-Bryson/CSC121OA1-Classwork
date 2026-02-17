8fc337eb592d147448e965b3b0ed129b45cc1554
Emily Bryson <emilymbryson@students.abtech.edu>
2026-02-17 12:03:48 -0500

    Add files via upload

---
+++ 4-2 Animals.py
@@ -0,0 +1,7 @@
+animals = ['gorilla', 'jaguar', 'african forest elephant']
+for animal in animals:
+    print(animal)
+for animal in animals:
+    print(f"{animal.title()}, live in African Forests.")
+
+print ("These animals all live in African Forests and are included in the animated movie tarzan.")
