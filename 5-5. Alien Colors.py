d30d116b390ad5333fc00a890e143bc05dfd6052
Emily Bryson <emilymbryson@students.abtech.edu>
2026-02-24 11:02:09 -0500

    Add files via upload

---
+++ 5-5. Alien Colors.py
@@ -0,0 +1,21 @@
+alien_color = 'green'
+if alien_color == 'green':
+    print("Version 1: Congratulations you just earned 5 points.")
+elif alien_color == 'yellow':
+    print("Version 1: Congratulations you earned 10 points.")
+else:
+    print("Version 1: Congratulations you just earned 15 points.")
+alien_color ='yellow'
+if alien_color == 'green':
+    print("Version 2: Congratulations you just earned 5 points.")
+elif alien_color == 'yellow':
+    print("Version 2: Congratulations you earned 10 points.")
+else:
+    print("Version 2: Congratulations you just earned 15 points.")
+alien_color = 'red'
+if alien_color == 'green':
+    print("Version 3: Congratulations you just earned 5 points.")
+elif alien_color == 'yellow':
+    print("Version 3: Congratulations you earned 10 points.")
+else:
+    print("Version 3: Congratulations you just earned 15 points.")
