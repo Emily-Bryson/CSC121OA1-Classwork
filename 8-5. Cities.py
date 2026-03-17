346ee9e0934927d585cb6c4882ee48d59f9b9466
Emily Bryson <emilymbryson@students.abtech.edu>
2026-03-17 12:33:57 -0400

    Add files via upload

---
+++ 8-5. Cities.py
@@ -0,0 +1,5 @@
+def describe_city(city_name, country_name="Argentina"):
+    print(f"{city_name} is in {country_name}.")
+describe_city("Buenos Aires")
+describe_city("San Juan")
+describe_city("Venice", "Italy")
